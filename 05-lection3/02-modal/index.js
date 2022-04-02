163c406f46306cb3eec9b67eeba228343d3bc96e
Gleb Krotov <glebkrotov@gmail.com>
2022-04-02 10:00:44 +0200

    Add modal (edits)

--- 05-lection3/02-modal/index.js
+++ 05-lection3/02-modal/index.js
@@ -2,21 +2,18 @@
     const close = document.getElementById('modal-close');
     const modal = document.getElementById('modal');
     const modalOpen = document.getElementById('modal-open');
-    const modalMask = document.getElementById('modal-mask');
 
-    if (!close || !modalOpen || !modal || !modalMask) {
+    if (!close || !modalOpen || !modal) {
         return;
     }
 
     modalOpen.addEventListener('click', () => {
         modal.style.display = 'block';
         document.body.overflow = 'hidden';
-        modalMask.style.display = 'block';
     });
 
     close.addEventListener('click', () => {
         modal.style.display = 'none';
         document.body.overflow = 'initial';
-        modalMask.style.display = 'none';
     });
 })();
